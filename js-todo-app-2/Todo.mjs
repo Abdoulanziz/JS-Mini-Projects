5836bf6d0abb271348b0bdfefda1acf829cd8395
Abdoulanziz Ally <35191747+Abdoulanziz@users.noreply.github.com>
2021-08-23 09:58:08 -0700

    OOP

--- js-todo-app-2/Todo.mjs
+++ js-todo-app-2/Todo.mjs
@@ -13,45 +13,38 @@ export class Todo {
         this.todoId = Todo.generateRandomStr(20);
         this.todoTitle = title;
         this.todoStatus = status;
-
-        // this.limitStrLen();
-        // this.caplitalizeFirstLetter();
-        // this.todoBuilder();
-        this.fetchTodosFromLocalStorage();
     }
 
-    fetchTodosFromLocalStorage() {
-        if (localStorage.getItem("Todos")) {
-            let todos = JSON.parse(localStorage.getItem("Todos"));
-            todos.forEach(todo => {
-                const todoObj = this.todoBuilder(todo);
-                document.querySelector(".todos").append(todoObj);
-            });
-        }
+    static fetchTodosFromLocalStorage() {
+        let todosList = localStorage.getItem("Todos2");
+        todosList = JSON.parse(todosList);
+        todosList.forEach(todo => {
+
+        });
+
     }
 
     deleteTodo(event) {
-        if (event.target.innerText == "Delete") {
-            todosList = JSON.parse(localStorage.getItem("Todos"));
+        // if (event.target.innerText == "Delete") {
+        //     todosList = JSON.parse(localStorage.getItem("Todos2"));
 
-            todosList.forEach(todo => {
-                if (event.target.parentElement.id === todo.id) {
-                    todosList.splice(todo, 1);
-                    event.target.parentElement.remove();
-                    localStorage.setItem("Todos", JSON.stringify(todosList));
-                }
-            });
-        }
+        //     todosList.forEach(todo => {
+        //         if (event.target.parentElement.id === todo.id) {
+        //             todosList.splice(todo, 1);
+        //             event.target.parentElement.remove();
+        //             localStorage.setItem("Todos2", JSON.stringify(todosList));
+        //         }
+        //     });
+        // }
     }
 
     todoBuilder() {
-        const value = this.caplitalizeFirstLetter(this.todoTitle);
         const todo = document.createElement("div");
         todo.setAttribute("id", this.todoId);
         todo.classList.add("todo");
         const title = document.createElement("h5");
         title.classList.add("title");
-        title.innerText = this.limitStrLen();
+        title.innerText = Todo.limitStrLen(Todo.caplitalizeFirstLetter(this.todoTitle));
         todo.append(title);
 
         const editBtn = document.createElement("h6");
@@ -64,22 +57,22 @@ export class Todo {
         deleteBtn.innerText = "Delete";
         todo.append(deleteBtn);
         todo.addEventListener("click", this.deleteTodo);
-        console.log(todo);
+        console.log(2, todo);
         return todo;
     }
 
-    limitStrLen() {
-        if (this.todoTitle.length >= Todo.todoTitleMaxLen) {
-            this.todoTitle = this.todoTitle.slice(0, Todo.todoTitleMaxLen);
-            console.log(`${this.todoTitle}...`);
-            return `${this.todoTitle}...`;
+    static limitStrLen(str) {
+        if (str.length >= Todo.todoTitleMaxLen) {
+            str = str.slice(0, Todo.todoTitleMaxLen);
+            console.log(`${str}...`);
+            return `${str}...`;
         }
-        return this.todoTitle;
+        return str;
     }
 
-    caplitalizeFirstLetter() {
-        const firstLetter = this.todoTitle.charAt(0);
-        const otherLetters = this.todoTitle.slice(1, this.todoTitle.length);
+    static caplitalizeFirstLetter(text) {
+        const firstLetter = text.charAt(0);
+        const otherLetters = text.slice(1, text.length);
         const newStr = firstLetter.toUpperCase() + otherLetters;
         return newStr;
     }
--- js-todo-app-2/main.mjs
+++ js-todo-app-2/main.mjs
@@ -1,17 +1,23 @@
 import { Todo } from './Todo.mjs';
 
 let todosList = new Array();
+Todo.fetchTodosFromLocalStorage();
 document.querySelector("#btn-save-todo").addEventListener("click", (event) => {
     let value = document.querySelector("#input-todo").value;
     if (value.length > 0) {
-
+        Todo.fetchTodosFromLocalStorage();
         const todo = new Todo(value, false);
 
         document.querySelector(".todos").append(todo.todoBuilder());
         document.querySelector("#input-todo").value = "";
 
-        todosList = JSON.parse(localStorage.getItem("Todos"));
-        todosList.push(todo);
-        localStorage.setItem("Todos", JSON.stringify(todosList));
+        if (localStorage.getItem("Todos2")) {
+            todosList = JSON.parse(localStorage.getItem("Todos2"));
+        } else {
+            todosList = [];
+        }
+
+        todosList.unshift({ id: todo.todoId, title: todo.todoTitle, status: todo.todoStatus });
+        localStorage.setItem("Todos2", JSON.stringify(todosList));
     }
 });
